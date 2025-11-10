3853b4c93418235113c06f54c464e42510431841
Rossileidys Silva <rossileidys.silva@epayco.com>
2025-11-10 09:26:32 -0400

    Reto#2: Repara los tests y valida resultados de búsqueda con Playwright

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,48 +1,36 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('/');
+  await page.goto('');
 });
 
 test('Realizar una busqueda que no tenga resultados', async ({ page }) => {
-  await page.getByRole('button').click();
 
+  await page.locator('button.DocSearch-Button').click();
   await page.getByPlaceholder('Search docs').click();
-
   await page.getByPlaceholder('Search docs').fill('hascontent');
-
-  expect(page.locator('.DocSearch-NoResults p')).toBeVisible();
-
-  expect(page.locator('.DocSearch-NoResults p')).toHaveText('No results for hascontent');
+  await expect(page.locator('.DocSearch-NoResults')).toBeVisible();
+  await expect(page.locator('.DocSearch-Title')).toContainText('hascontent');
 
 })
 
 test('Limpiar el input de busqueda', async ({ page }) => {
-  await page.getByRole('button', { name: 'Search' }).click();
-
+ 
+  await page.locator('button.DocSearch-Button').click();
   const searchBox = page.getByPlaceholder('Search docs');
-
   await searchBox.click();
-
   await searchBox.fill('somerandomtext');
-
-  await expect(searchBox).toHaveText('somerandomtext');
-
   await page.getByRole('button', { name: 'Clear the query' }).click();
-
   await expect(searchBox).toHaveAttribute('value', '');
 });
 
 test('Realizar una busqueda que genere al menos tenga un resultado', async ({ page }) => {
-  await page.getByRole('button', { name: 'Search ' }).click();
-
+  
+  await page.locator('button.DocSearch-Button').click();
   const searchBox = page.getByPlaceholder('Search docs');
-
   await searchBox.click();
-
-  await page.getByPlaceholder('Search docs').fill('havetext');
-
-  expect(searchBox).toHaveText('havetext');
+  await searchBox.fill('havetext');
+  await expect(searchBox).toHaveValue('havetext');
 
   // Verity there are sections in the results
   await page.locator('.DocSearch-Dropdown-Container section').nth(1).waitFor();
